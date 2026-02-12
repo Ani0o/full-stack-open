e9f9e2d316f74118b6b5260f35d29a3ab8452a4d
Ani <205335742+Ani0o@users.noreply.github.com>
2026-02-12 19:21:55 +0530

    completed exercise 1.10

--- part1/unicafe/exercise/src/App.jsx
+++ part1/unicafe/exercise/src/App.jsx
@@ -1,5 +1,21 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+  return (
+    <>
+      <button onClick={props.onClick}>{props.text}</button>
+    </>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <div>
+      {props.text} {props.value} {props.sign}
+    </div>
+  )
+}
+
 const Statistics = (props) => {
 
   const all = props.good + props.neutral + props.bad
@@ -17,14 +33,14 @@ const Statistics = (props) => {
     <div>
       <h1>statistics</h1>
 
-      good {props.good} <br />
-      neutral {props.neutral} <br />
-      bad {props.bad} <br />
-      all {all} <br />
+      <StatisticLine text={"good"} value={props.good} />
+      <StatisticLine text={"neutral"} value={props.neutral} />
+      <StatisticLine text={"bad"} value={props.bad} />
+      <StatisticLine text={"all"} value={all} />
 
-      average {(props.good - props.bad) / (all)} <br />
+      <StatisticLine text={"average"} value={(props.good - props.bad) / (all)} />
 
-      positive { (props.good / (all)) * 100} %
+      <StatisticLine text={"positive"} value={(props.good / (all)) * 100} sign={"%"} />
     </div>
   )
 }
@@ -51,9 +67,9 @@ const App = () => {
     <div>
       <h1>give feedback</h1>
 
-      <button onClick={handleGood}>good</button>
-      <button onClick={handleNeutral}>neutral</button>
-      <button onClick={handleBad}>bad</button>
+      <Button onClick={handleGood} text={"good"} />
+      <Button onClick={handleNeutral} text={"neutral"} />
+      <Button onClick={handleBad} text={"bad"} />
 
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
