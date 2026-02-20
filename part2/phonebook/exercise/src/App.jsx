f142161031f03a67d35252d8ed61d8a07f67f1e1
Ani <205335742+Ani0o@users.noreply.github.com>
2026-02-20 21:03:08 +0530

    completed exercise 2.8

--- part2/phonebook/exercise/src/App.jsx
+++ part2/phonebook/exercise/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleClick = (event) => {
     event.preventDefault()
@@ -17,14 +18,20 @@ const App = () => {
     
     
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
-  const handleChange = (event) => {
+  const handleName = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumber = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -32,14 +39,17 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: <input value={newName} onChange={handleName} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumber}/>
         </div>
         <div>
           <button type="submit" onClick={handleClick}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name}</div>)}
+      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
     </div>
   )
 }
