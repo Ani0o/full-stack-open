b00205388402964ddf14b20d718b1b4dcc38da08
Ani <205335742+Ani0o@users.noreply.github.com>
2026-03-11 22:27:25 +0530

    completed exercise 3.2

--- part3/index.js
+++ part3/index.js
@@ -28,6 +28,13 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const numberOfPersons = persons.length
+    const currentDate = new Date()
+
+    response.send(`Phonebook has info for ${numberOfPersons} people <br /> <br />` + currentDate)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
